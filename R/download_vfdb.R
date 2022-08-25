3dfb602090b4409798091ec06a12426fc67c22d9
MBonjean <67752577+maous1@users.noreply.github.com>
2022-08-25 14:23:56 +0200

    Update download_vfdb.R

--- R/download_vfdb.R
+++ R/download_vfdb.R
@@ -8,6 +8,6 @@
 download_vfdb <- function(path)
 {
   download.file(url = "http://www.mgc.ac.cn/VFs/Down/VFDB_setA_nt.fas.gz",
-                destfile = paste0(path,"/VFDB_setB_nt.fas.gz"))
-  gunzip(filename = paste0(path,"/VFDB_setB_nt.fas.gz"), remove=T)
+                destfile = paste0(path,"/VFDB_setA_nt.fas.gz"))
+  gunzip(filename = paste0(path,"/VFDB_setA_nt.fas.gz"), remove=T)
 }
